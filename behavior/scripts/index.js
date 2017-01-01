80e650eb73abfe798c7a77ef76b00ec29b800b20
teroqim <teroqim@gmail.com>
2017-01-01 21:09:12 +0100

    Bug fixes

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,13 +1,13 @@
 'use strict'
-const Audiosearch = require('.lib/audiosearch')
+const Audiosearch = require('./lib/audiosearch')
 const audiosearch = new Audiosearch(process.env.AUDIOSEARCH_APP_ID, process.env.AUDIOSEARCH_SECRET)
 
 function searchShows(query, callback){
   audiosearch.searchShows(query)
-    .then(shows => {
-      console.log(shows)
+    .then(response => {
+      // console.log('search: '+response)
       if (callback) {
-        callback(shows)
+        callback(response.results)
       }
     })
     .catch(ex => {
@@ -20,10 +20,10 @@ function searchShows(query, callback){
 
 function searchSimilarShows(show, callback){
   audiosearch.getRelated(show.id)
-    .then(shows => {
-      console.log(shows)
+    .then(response => {
+      // console.log(response)
       if (callback) {
-        callback(shows)
+        callback(response)
       }
     })
     .catch(ex => {
@@ -66,8 +66,8 @@ exports.handle = (client) => {
 
   const findShow = client.createStep({
     extractInfo() {
-      let podcastTitle = firstOfEntityRole(client.getMessagePart(), 'podcast_title')
-
+      let podcastTitle = client.getFirstEntityWithRole(client.getMessagePart(), 'podcast_title')
+      console.log('found title in extract:', podcastTitle)
       if (podcastTitle) {
         client.updateConversationState({
           requestedPodcastTitle: podcastTitle,
@@ -80,10 +80,13 @@ exports.handle = (client) => {
     },
 
     prompt(done) {
-      searchShows(client.getConversationState().requestedPodcastTitle, shows => {
+      console.log('findShow prompt')
+      searchShows(client.getConversationState().requestedPodcastTitle.value, shows => {
+        console.log('findShow callback:', shows[0].title)
         client.updateConversationState({
           show: shows[0],
         })
+        client.done()
         done()
       })
     }
@@ -91,18 +94,21 @@ exports.handle = (client) => {
 
   const recommendSimilar = client.createStep({
     satisfied() {
-      return Boolean(client.getConversationState().recommendedShows)
+      return false
     },
 
     prompt(done) {
+      console.log('recommendSimilar prompt')
       searchSimilarShows(client.getConversationState().show, shows => {
+        console.log('recommendSimilar callback', shows)
         client.addResponse('3_podcasts', {
-          'podcast_title#1': shows[0],
-          'podcast_title#2': shows[1],
-          'podcast_title#3': shows[2],
+          'podcast_title#1': shows[0].title,
+          'podcast_title#2': shows[1].title,
+          'podcast_title#3': shows[2].title,
         })
 
         client.done()
+        done()
       })
     }
   })
